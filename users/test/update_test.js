5236472bdd8dc7b0db0db14bf3e0953431e0512b
Du <Sampan0103314@gmail.com>
2020-03-20 00:07:23 -0400

    mongo update operator and mongoose validation

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach(done => {
-    joe = new User({ name: 'Joe' });
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save().then(() => done());
   });
 
@@ -64,5 +64,15 @@ describe('Updating records', () => {
   it('a model class can find a record with an Id and update', done => {
     //update one record
     assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
+  });
+
+  it('A user can have their postcount incremented by 1', done => {
+    //using mongo update operator $inc is more efficient and performance while updating a bunch of records.
+    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then(user => {
+        assert(user.postCount === 1);
+        done();
+      });
   });
 });
