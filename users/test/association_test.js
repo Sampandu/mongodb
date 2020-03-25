368c31d37c9724c79312c3c520706c1a93402970
Du <Sampan0103314@gmail.com>
2020-03-25 00:32:34 -0400

    loaded deeply nested association

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -26,9 +26,32 @@ describe('association', () => {
   //adding .only will execute this test suit only
   it('saves a relation between a user and a blogpost', done => {
     User.findOne({ name: 'Joe' })
-      .populate('blogPosts') //enhance the query and fetch the associated blogPost collection
+      .populate('blogPosts') //it is the property blogPosts not the collection blogPost, populate enhances the query and fetch the associated collection
       .then(user => {
         assert(user.blogPosts[0].title === 'JS is Great.');
+        done();
+      });
+  });
+
+  //caution: the number of the associations to load up, the more the number of the associations to load up, the easier to bog down the duration of the query
+  it('saves a full relation graph', done => {
+    User.findOne({ name: 'Joe' })
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'user',
+            model: 'user',
+          },
+        },
+      })
+      .then(user => {
+        assert(user.name === 'Joe');
+        assert(user.blogPosts[0].title === 'JS is Great.');
+        assert(user.blogPosts[0].comments[0].content === 'Agree on that');
+        assert(user.blogPosts[0].comments[0].user.name === 'Joe');
         done();
       });
   });
